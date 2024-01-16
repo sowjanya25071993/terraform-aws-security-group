6dfd3b8a4bcc03fb6420efccad670c44d444544b
sowji <jujjurisowjanya@gmail.com>
2024-01-16 18:45:46 +0530

    new

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+    }
+variable "environment" {
+    }
+variable "sg_name" {
+    }
+variable "common_tags" {
+    default = {}
+    type = map
+}
+variable "sg_tags" {
+    default = {}
+    type = map
+}
+variable "vpc_id" {
+    }
+variable "sg_description" {
+    default = ""
+    type = string
+}
+variable "sg_ingress_rules" {
+    type = list
+    default = []
+}
